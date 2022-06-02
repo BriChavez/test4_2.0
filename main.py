e888cd78272273f41e72ef9af7a10c0fb9f28638
BriChavez <brianachavez@gmail.com>
2022-06-01 23:31:49 -0700

    fix few minor typos in readme, fixed a few missing lines/words from main.py

--- main.py
+++ main.py
@@ -26,52 +26,56 @@ class DataLoader():
         """prints the head of the dataframe to console"""
         print(self.df.head())
 
-    # utility to expose the info method of our data frame onto our base class
     def info(self):
+        # utility to expose the info method of our data frame onto our base class
         """bind info to our dataframe"""
         return self.df.info
 
-        # if our data doesn't already have one, this is a function that creates an index from a concat of columns as a list as well is an optional name for index
-    def add_index(self, col_list, index_name="index") -> None:
-        """create an index column by concating s list of columns into a string"""
+    def add_index(self, index_name="index") -> None:
+        """create an index column"""
+        # if our data doesn't already have one, this is a function that creates an index
         df = self.df
         # summon our logger buddy so he can later let us know when we make an index and what we named him
-        logger.info(f"\tAdding inex {index_name}")
-        self.df = df
+        logger.info(f"\tAdding index {index_name}")
+        # concats cols in col_list into an index column to serve us faithfully as our loyal primary key
+        df[index_name] = df[col_list].apply(lambda row: "-".join(row.values.astype(str)), axis=1)
+        # replaces the index of the dataframe to our concated columns
+        df.set_index(index_name, inplace=True)
         # this gives us the ability to add an index when one is not present
+        self.df = df
 
     def sort(self, column_name:str) -> None:
         """sort the data by a later specified column"""
         df = self.df
         df.sort_values(column_name)
-        self.df = df
 
     def merge(self, df, left_on, right_on, join_cols, how = 'left'):
-        # merge df from multiple csvs on later specified columns
-        df = self.df
+        """merge df from multiple CSVs on later specified columns"""
         df = pd.merge(left = self.df, right = df[join_cols], left_on = left_on, right_on = right_on, how = how)
         
 
-    def load_to_db(self, db_engine, db_table_name:str) -> None:
+    def load_to_db(self, db_table_name:str) -> None:
         """load dataframe into a database table"""
-        df = self.df
         self.df.to_sql(name=db_table_name, con=self.engine, if_exists='replace', chunksize=2000)
-        self.df = df
 
 
 def db_engine(db_host: str, db_user: str, db_pass: str, db_name: str = "spotify") -> sa.engine.Engine:
-        """Using SqlAlchemy, create a database engine and return it"""
-        #create enginge using sqlalchmey
+        """use SqlAlchemy to create a database engine and return it"""
+        # create engine using SqlAlchmey
         engine = sa.create_engine(f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}', future = True)
+        # binds metadata to the engine
         metadata = MetaData(bind=engine)
+        # sets the connect to the engine to a variable to call upon later
+        conn = engine.connect()
         return engine
 
 
 def db_create_tables(db_engine, drop_first = False) -> None:
+    """create tables """
     meta = sa.MetaData(bind=db_engine)
-    #define columns from the artists table
-    artists_table = Table("artists",
-                        Column('artist_poularity', Numeric),
+    # define columns from the artists table
+    artists_table = Table("artists", meta,
+                        Column('artist_popularity', Numeric),
                         Column('followers', Numeric),
                         Column('genres', String(10240)),
                         Column('id', Numeric, primary_key=True),
@@ -79,11 +83,10 @@ def db_create_tables(db_engine, drop_first = False) -> None:
                         Column('track_id', String(256)),
                         Column('track_name_prev', String(256)),
                         Column('type', String(256)),
-                        extend_existing=True
-                        )
-    # ,artist_popularit~y,followers,genres,id,name,track_id,track_name_prev,type
-    #define columns from the albums table
-    albums_table = Table("albums",
+                        extend_existing=True)
+
+    # define columns from the albums table
+    albums_table = Table("albums", meta,
                         Column('album_type', String(256)),
                         Column('artist_id', String(256)),
                         Column('available_markets', String(10240)),
@@ -99,25 +102,25 @@ def db_create_tables(db_engine, drop_first = False) -> None:
                         Column('track_name_prev', String(256)),
                         Column('uri', String(256)),
                         Column('type', String(256)),
-                        extend_existing=True
-                        )
+                        extend_existing=True)
 
-    #drop tables if drop_first = True
+    # drop tables if drop_first = True
     if drop_first:
         meta.drop_all()
 
-    #create tables
+    # create tables
     meta.create_all(checkfirst=True)
     
 
-
 def main():
     """Pipeline Orchestratation method."""
-    # create a data loader for albums and artists
+    # create a data loader for albums
     album_data = DataLoader('./data/spotify_albums.csv')
+    # create a data loader for artists
     artist_data = DataLoader('./data/spotify_artists.csv')
-    # print the first 10 rows of each set
+    # print the first 10 rows of album set
     album_data.head()
+    # print the first 10 rows of artist set
     artist_data.head()
     # set index of albums to three of the columns
     album_data.add_index('index', ['artist_id', 'id', 'release_date'])
@@ -129,8 +132,9 @@ def main():
     engine = db_engine('127.0.0.1:3306', 'root', 'mysql')
     # create db meta date table
     db_create_tables(engine, drop_first = True)
-    # load them both into a db
+    # load artists into a db
     artist_data.load_to_db(engine, 'artists')
+    # load albums into a db
     album_data.load_to_db(engine, 'albums')
     # merge them both into one super table
     artist_data.merge(df = artist_data, left_on = artist_data, right_on = album_data, join_cols = 'id', sort = 'id', how = 'left')
